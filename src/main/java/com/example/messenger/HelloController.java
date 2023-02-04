15d557b787e2c6d7c8b124ecf9c2d96a5e3b790b
rinsarmu <roberaensermu@gmail.com>
2023-02-04 14:41:34 +0300

    Handling controller

--- src/main/java/com/example/messenger/HelloController.java
+++ src/main/java/com/example/messenger/HelloController.java
@@ -1,14 +1,68 @@
 package com.example.messenger;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
-public class HelloController {
-    @FXML
-    private Label welcomeText;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+public class HelloController implements Initializable {
+   private Button button_send;
+
+   private TextField tf_message;
+   private VBox vbox_message;
+   private ScrollPane sp_main;
+   private Server server;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        try{
+            server = new Server(new ServerSocket(1234));
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Error creating server.");
+        }
+
+//        for automatical scroll message
+        vbox_message.heightProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observableValue, Number number, Number newValue) {
+                sp_main.setVvalue((Double) newValue);
+            }
+        });
+
+        //It's thread, which receive contionusly
+        server.receiveMessageFromClient(vbox_message);
+
+        button_send.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                String messageToSend = tf_message.getText();
+                if(!messageToSend.isEmpty()) {
+                    HBox hBox = new HBox(); //box message
+                    hBox.setAlignment(Pos.CENTER_RIGHT);
+                    hBox.setPadding((new Insets(5,5,5,10)));
+
+                    //text object which display our message
+
+                }
+            }
+        });
     }
+
 }

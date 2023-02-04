041792e38e1ef9c59a6bf50f5e66ad4f0df1150c
rinsarmu <roberaensermu@gmail.com>
2023-02-04 15:10:20 +0300

    Handling controller and creating server

--- src/main/java/com/example/messenger/HelloController.java
+++ src/main/java/com/example/messenger/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.messenger;
 
+import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
@@ -14,6 +15,9 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 
 import java.io.IOException;
 import java.net.ServerSocket;
@@ -59,10 +63,54 @@ public class HelloController implements Initializable {
                     hBox.setPadding((new Insets(5,5,5,10)));
 
                     //text object which display our message
+                    Text text = new Text(messageToSend);
+                    TextFlow textFlow = new TextFlow(text); // to wrap our text
+
+                    textFlow.setStyle("-fx-color:rgb(239,242,255) " +
+                            "-fx-background-color:rgb(15,125,242) " +
+                            "-fx-background-radius: 20px");
+
+                    textFlow.setPadding(new Insets(5,10,5,10));
+                    text.setFill(Color.color(0.934, 0.945, 0.956));
+
+                    hBox.getChildren().add(textFlow);
+
+                    server.sendMessageToClient(messageToSend);
+                    tf_message.clear();
+                    ;
+
 
                 }
             }
         });
+
+
+    }
+    public static void addLabel(String messageFromClient, VBox vBox) {
+        HBox hBox = new HBox(); //box message
+        hBox.setAlignment(Pos.CENTER_LEFT);
+        hBox.setPadding((new Insets(5,5,5,10)));
+
+        //text object which display our message
+        Text text = new Text(messageFromClient);
+        TextFlow textFlow = new TextFlow(text); // to wrap our text
+
+        textFlow.setStyle("-fx-background-color:rgb(233,233,235) " +
+                "-fx-background-radius: 20px");
+
+        textFlow.setPadding(new Insets(5,10,5,10));
+//        text.setFill(Color.color(0.934, 0.945, 0.956));
+
+        hBox.getChildren().add(hBox);
+        //to update our ui
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                vBox.getChildren().add(hBox);
+            }
+        });
+
+
     }
 
 }
--- src/main/java/com/example/messenger/Server.java
+++ src/main/java/com/example/messenger/Server.java
@@ -1,4 +1,28 @@
 package com.example.messenger;
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+
+    private ServerSocket serverSocket;
+    private Socket socket;
+
+    private BufferedReader bufferedReader;
+    private BufferedWriter bufferedWriter;
+
+    public Server(ServerSocket serverSocket) {
+        try{
+            this.serverSocket = serverSocket;
+            this.socket = serverSocket.accept(); //server object to return server. it's blocking agent
+
+            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        }  catch(IOException e){
+            System.out.println("Error creating server");
+            e.printStackTrace();
+
+         }
+    }
 }
